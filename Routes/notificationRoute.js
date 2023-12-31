3871d02ad7723a0be6f3ea20c270e6f3f2e2af1a
Chirag <chiragmakwana1807@gmail.com>
2023-12-31 10:44:13 +0530

    small change

--- Routes/notificationRoute.js
+++ Routes/notificationRoute.js
@@ -3,6 +3,6 @@ const notificationController = require('../Controller/Notification/notification'
 const { authenticateToken } = require('../Middleware/auth');
 const router = express.Router();
 
-router.post('/create_request_meeting', authenticateToken, notificationController.getNotification);
+router.post('/get_notifications', authenticateToken, notificationController.getNotification);
 
 module.exports = router;
