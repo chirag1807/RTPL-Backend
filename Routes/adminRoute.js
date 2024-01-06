437f366c22a53346d17543c17bb5b8bfe9cc19df
Ridham <115993280+rythm01@users.noreply.github.com>
2024-01-06 17:21:13 +0530

    Modifications

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const adminController = require('../Controller/Personnel/adminCtrl');
-const { isAdmin } = require('../Middleware/auth');
+const { isSuper } = require('../Middleware/auth');
 const { upload } = require('../utils/multer');
 const router = express.Router();
 
 router.post('/addAdmin',
-    // isAdmin,
     upload.fields([
         { name: 'empAadharCard', maxCount: 1 },
         { name: 'empIdCard', maxCount: 1 },
         { name: 'empProfileImg', maxCount: 1 },
     ]),
+    isSuper,
     adminController.addAdmin);
 router.post('/addReceptionist',
-    isAdmin,
     upload.fields([
         { name: 'empAadharCard', maxCount: 1 },
         { name: 'empIdCard', maxCount: 1 },
         { name: 'empProfileImg', maxCount: 1 },
     ]),
+    isSuper,
     adminController.addReceptionist);
-router.get('/getAdminList', isAdmin, adminController.getAdmins);
-router.get('/getDataList', isAdmin, adminController.getAllData);
+router.get('/getAdminList', isSuper, adminController.getAdmins);
+router.get('/getDataList', isSuper, adminController.getAllData);
 router.put('/:id',
-    isAdmin,
+    isSuper,
     adminController.updateAdmin);
 router.delete('/:id',
-    isAdmin,
+    isSuper,
     adminController.deleteAdmin);
 
 module.exports = router;
