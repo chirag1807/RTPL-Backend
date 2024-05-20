1dbb8ba36d84538f07651f2fcc0410b28001e693
kokhp <himanshu.dudhat15@gmail.com>
2024-05-20 16:29:11 +0530

    Updated size of req

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(
   })
 );
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false,limit: '50mb' }));
+app.use(express.json({limit: '150mb'}));
 app.use(bodyParser.json({ limit: '150mb' }));
 app.use("/uploads", express.static("uploads"));
 
